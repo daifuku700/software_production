98d33627ddb520034a226952b7273cb03abd36e9
kwkmhrk24 <kwkmhrk24@ruri.waseda.jp>
2024-06-08 11:32:37 +0900

    maid_send

--- client/Client.java
+++ client/Client.java
@@ -12,9 +12,47 @@ public class Client {
             System.out.println("socket = " + socket);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+
+            // ファイル送信の意思表示
+            Send(out);
+
+            // サーバーからの応答を確認した後ファイルを送信
+            String response = in.readLine();
+            if ("Ready".equals(response)) {
+                SendFile(socket, "audio.wav"); // ファイル名を指定
+            }
+
+            
         } finally {
             System.out.println("closing...");
             socket.close();
+        }
+    }
+
+    // ファイル送信の意思表示
+    private static void Send(PrintWriter out) {
+        out.println("Send");
+    }
+
+    // 音声ファイルの送信
+    private static void SendFile(Socket socket, String filePath) {
+        byte[] buffer = new byte[512]; // ファイル送信時のバッファ
+        
+        try (InputStream inputStream = new FileInputStream(filePath);
+            OutputStream outputStream = socket.getOutputStream()) {
+
+            // ファイルをストリームで送信
+            int fileLength;
+            while ((fileLength = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, fileLength);
+            }
+
+            // 終了処理
+            outputStream.flush();
+            System.out.println("File sent successfully.");
+
+        } catch (IOException e) {
+            System.err.println("File send error: " + e.getMessage());
         }
     }
 }
--- server/ServerFunc.java
+++ server/ServerFunc.java
@@ -1,11 +1,38 @@
 package server;
 
-import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
 public class ServerFunc extends Thread {
     public ServerFunc() {
         super();
     };
 
-    
+    // ファイルを受信する関数
+    public void receiveFile(Socket socket) { 
+        String outputFilepath = "server/received_audio.wav"; // 受信したファイルの保存先
+        byte[] buffer = new byte[512]; // ファイル受信時のバッファ
+
+        try {
+            InputStream inputStream = socket.getInputStream();
+            FileOutputStream outputStream = new FileOutputStream(outputFilepath);
+
+            // ファイルをストリームで受信
+            int fileLength;
+            while ((fileLength = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, fileLength);
+            }
+
+            // 終了処理
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+            System.out.println("File received successfully.");
+
+        } catch (IOException e) {
+            System.err.println("File receive error: "  + e.getMessage());
+        }
+    }
 }
