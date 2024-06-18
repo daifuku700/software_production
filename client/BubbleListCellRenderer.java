08f88365f0d85578389af8b99880576864ecf6d5
monchy1017 <monchy1017.mao@gmail.com>
2024-06-18 14:58:38 +0900

    sendbuton追加

---
+++ client/BubbleListCellRenderer.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+public class BubbleListCellRenderer extends JLabel implements ListCellRenderer<Object> {
+    public BubbleListCellRenderer() {
+        setOpaque(true);
+    }
+
+    @Override
+    public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected,
+            boolean cellHasFocus) {
+        setText(value.toString());
+
+        // テキストを丸い枠で囲むための設定
+        setBorder(BorderFactory.createMatteBorder(10, 10, 10, 10, Color.WHITE));
+        setOpaque(true);
+        setBackground(Color.LIGHT_GRAY);
+        setForeground(Color.BLACK);
+
+        if (isSelected) {
+            setBackground(Color.BLUE);
+            setForeground(Color.WHITE);
+        } else {
+            setBackground(Color.LIGHT_GRAY);
+            setForeground(Color.BLACK);
+        }
+
+        return this;
+    }
+}
--- client/Display.java
+++ client/Display.java
@@ -1,30 +1,23 @@
 package client;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
-import javax.swing.border.LineBorder;
 
-public class Display extends JFrame implements ActionListener {
-    private JPanel mainPanel;
+public class Display extends JFrame {
     private JButton recordButton;
+    private JButton sendButton;
     private JList<String> recordingList;
     private DefaultListModel<String> listModel;
-    private JLabel l1, l2;
-    private JButton b1, b2;
 
     public Display() {
         super("音声ソフトアプリ");
@@ -38,8 +31,6 @@ public class Display extends JFrame implements ActionListener {
     }
 
     private void initComponents() {
-        mainPanel = new JPanel(new BorderLayout());
-
         // リストモデルとリスト
         listModel = new DefaultListModel<>();
         recordingList = new JList<>(listModel);
@@ -49,35 +40,13 @@ public class Display extends JFrame implements ActionListener {
         JPanel buttonPanel = new JPanel(new FlowLayout());
         recordButton = new JButton("録音");
         buttonPanel.add(recordButton);
+        sendButton = new JButton("送信");
+        buttonPanel.add(sendButton);
 
-        // ButtonLabelSampleのコンポーネントを作成
-        JPanel buttonLabelPanel = new JPanel();
-        GridLayout layout = new GridLayout(2, 2);
-        buttonLabelPanel.setLayout(layout);
-
-        l1 = new JLabel("label1", SwingConstants.CENTER);
-        l2 = new JLabel("label2", SwingConstants.CENTER);
-        l1.setBorder(new LineBorder(Color.BLACK, 2));
-        l2.setBorder(new LineBorder(Color.BLACK, 2));
-        l1.setBackground(Color.LIGHT_GRAY);
-        l2.setBackground(Color.LIGHT_GRAY);
-        l1.setOpaque(true);
-        l2.setOpaque(true);
-
-        b1 = new JButton("button1");
-        b2 = new JButton("button2");
-        b1.addActionListener(this);
-        b2.addActionListener(this);
-
-        buttonLabelPanel.add(l1);
-        buttonLabelPanel.add(b1);
-        buttonLabelPanel.add(l2);
-        buttonLabelPanel.add(b2);
-
+        // メインパネルにコンポーネントを追加
+        JPanel mainPanel = new JPanel(new BorderLayout());
         mainPanel.add(scrollPane, BorderLayout.CENTER);
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
-        mainPanel.add(buttonLabelPanel, BorderLayout.NORTH);
-
         add(mainPanel);
 
         // ボタンの動作を設定
@@ -85,6 +54,11 @@ public class Display extends JFrame implements ActionListener {
             public void actionPerformed(ActionEvent e) {
                 listModel.addElement("録音 " + (listModel.getSize() + 1));
                 // 録音処理を実装
+            }
+        });
+        sendButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // 送信処理を実装
             }
         });
 
@@ -97,27 +71,6 @@ public class Display extends JFrame implements ActionListener {
                 }
             }
         });
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String command = e.getActionCommand();
-        if ("button1".equals(command)) {
-            String text = l1.getText();
-            if ("label1".equals(text)) {
-                l1.setText(text.toUpperCase());
-            } else {
-                l1.setText(text.toLowerCase());
-            }
-        } else if ("button2".equals(command)) {
-            if (l2.getBackground().equals(Color.LIGHT_GRAY)) {
-                l2.setBackground(Color.RED);
-                l2.setOpaque(true);
-            } else {
-                l2.setBackground(Color.LIGHT_GRAY);
-                l2.setOpaque(true);
-            }
-        }
     }
 
     public static void main(String[] args) {
